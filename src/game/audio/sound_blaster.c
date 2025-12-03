8b7533233c1160147fc12953fc1cf419fbd3126a
mdeter777 <eter_wheyy@proton.me>
2025-12-03 13:30:41 +0100

    Quick commit on 2025-12-03 13:30:41

--- src/game/audio/sound_blaster.c
+++ src/game/audio/sound_blaster.c
@@ -1,10 +1,13 @@
 #include "sound_blaster.h"
+#include "game/loaders/wave_file.h"
 #include <stdlib.h>
 #include <AL/al.h>
 #include <AL/alc.h>
 
 // NOTE: Only works on arrays, not pointers!
 #define ARRAY_SIZE(arr) (sizeof(arr) / sizeof(arr[0]))
+
+static ALuint g_al_buffer;
 
 static int internal_audio_openal_init(const char* device)
 {
@@ -23,6 +26,8 @@ static int internal_audio_openal_init(const char* device)
 
 static void internal_audio_openal_shutdown(void)
 {
+    alDeleteBuffers(1, &g_al_buffer);
+
     ALCdevice* Device;
     ALCcontext* Context;
     Context=alcGetCurrentContext();
@@ -32,12 +37,27 @@ static void internal_audio_openal_shutdown(void)
     alcCloseDevice(Device);
 }
 
+void sound_blaster_play_sound()
+{
+
+}
+
+void sound_blaster_load_wav(WAVFile* wave)
+{
+ //   alBufferData(g_al_buffer, );
+}
+
 
 int sound_blaster_init(void)
 {
     if (internal_audio_openal_init(NULL)) {
         return 1;
     }
+
+    alGetError();
+    alGenBuffers(1, &g_al_buffer);
+
+   // alBufferData(g_al_buffer, AL_FORMAT_MONO16, pcmData, dataSize, sampleRate);
 
     return 0;
 }
--- src/game/loaders/wave_file.c
+++ src/game/loaders/wave_file.c
@@ -1,4 +1,5 @@
 #include "wave_file.h"
+#include "platform/file_read.h"
 #include <string.h>
 #include <stdio.h>
 #include <stdlib.h>
